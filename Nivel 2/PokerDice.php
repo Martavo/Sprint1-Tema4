f1b9d8e506828d6c5d0c2d491a1081d62d733d09
Marta Villalba <marta.villalba.orta@gmail.com>
2023-11-24 16:17:26 +0100

    Ejecución del nivel 2

---
+++ Nivel 2/PokerDice.php
@@ -0,0 +1,28 @@
+<?php
+class PokerDice{
+    // Atributos
+    public $dado = array("As", "K", "Q", "J", 7, 8);
+    public $recuentoTiradas = 0;
+    public static $tiradaActual;
+
+
+
+    // Función para tirar un dado de manera aleatoria
+    public function throw() {
+        $valorAleatorio = array_rand($this->dado);
+        $this->tiradaActual = $this->dado[$valorAleatorio];
+        $this->recuentoTiradas++;
+    }
+
+    // Función para indicar el valor de la última tirada
+    public function shapeName(){
+        return $this->tiradaActual;
+    }
+
+    // Mostrar el nombre total de tiradas
+    public function getTotalThrows(){
+        return $this->recuentoTiradas;
+    }
+
+}
+?>
---
+++ Nivel 2/index.php
@@ -0,0 +1,53 @@
+<?php
+require_once "PokerDice.php";
+
+// Inicializar instancia de PokerDice
+$dado = new PokerDice();
+
+// Procesar la opción del usuario
+$opcion = $_POST['opcion'];
+
+switch ($opcion) {
+    case "Tirar 1 dado":
+        $dado->throw();
+        echo "Valor de la tirada: " . $dado->shapeName(). "\n";
+        break;
+
+    case "Tirar los 5 dados":
+        for ($i = 0; $i < 5; $i++) {
+            $dado->throw(); // Usar la misma instancia para todos los dados
+            echo "Dado $i: " . $dado->shapeName() . "\n";
+        }
+        break;
+
+    case "Salir del juego":
+        echo "El recuento total de tiradas es: " . $this -> getTotalThrows();
+        echo "<br>";
+        echo "Saliendo del juego. ¡Hasta luego!\n";
+        break;
+
+    default:
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menú</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>Seleccione qué quiere realizar</label>
+        <p></p>
+        <select name="opcion">
+            <option value="Tirar 1 dado">Tirar 1 dado</option>
+            <option value="Tirar los 5 dados">Tirar los 5 dados</option>
+            <option value="Salir del juego">Salir del juego</option>
+        </select>
+        <p></p>
+        <button type="submit">Confirmar</button>
+    </form>
+</body>
+</html>
