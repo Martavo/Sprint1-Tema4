af02bb66f6ac50c8b3920ff9878432282e5eb035
Marta Villalba <marta.villalba.orta@gmail.com>
2023-11-25 15:31:42 +0100

    Menú finalizado y apartado html hecho

--- Nivel 3/menu.php
+++ Nivel 3/menu.php
@@ -1,10 +1,10 @@
 <?php
-include_once('Cine.php');
-include_once('Pelicula.php');
+require_once "Cine.php";
+require_once "Pelicula.php";
 
 //Instanicar los cines:
 $cine1 = new Cine("Ocine", "Vendrell");
-$cine2 = new Cine("Cinesa", "Terrassa")
+$cine2 = new Cine("Cinesa", "Terrassa");
 
 // Instanciar las películas y agregarlas a los cines
 $cine1->agregarPelicula(new Pelicula("Siete reyes deben morir", "111", "Edward Bazalgette"));
@@ -17,30 +17,34 @@ $cine2->agregarPelicula(new Pelicula("Oppenheimer", "180", "Christopher Nolan"))
 $cines = [$cine1, $cine2];
 
 //Menu de opciones
-do {
-    
-    switch ($eleccion) {
-        case 0:
-            echo "Gracias por usar la aplicación" . PHP_EOL;
-            break;
+$opcion = $_POST['opcion'];
+switch ($opcion) {
+    case "Salir de la aplicación":
+        echo "Gracias por usar la aplicación";
+        break;
 
-        case 1:
-            mostrarTodosCines($cines);
-            break;
+    case "Mostrar peliculas":
+        mostrarTodosCines($cines);
+        break;
     
-        case 2:
-            buscarPeliculaLarga($cines);
-            break;
+    case "Información de la pelicula más larga":
+        buscarPeliculaLarga($cines);
+        break;
 
-        case 3:
-           
-            break;
-    
-        default:
-            echo "Opción no valida";
-    }
-
-} while ($eleccion != 0);
+    case "Buscar pelicula por director":
+        $director = $_POST['director'];
+        switch ($director) {
+            case "Edward Bazalgette":
+            case "David Wachtenheim":
+            case "Lucía Alemany":
+            case "Christopher Nolan":
+                buscarPorDirector($cines, $director);
+                break;
+            default:
+        }
+        break;
+    default:       
+}
 
 // Función para mostrar todos los datos
 function mostrarTodosCines($cines) {
@@ -65,7 +69,7 @@ function buscarPeliculaLarga($cines) {
 function buscarPorDirector($cines, $nombreDirector) {
     $peliculasEncontradas = [];
     foreach ($cines as $cine) {
-        foreach ($cine->lista_peliculas as $pelicula) {
+        foreach ($cine->getListaPeliculas() as $pelicula) {
             if ($pelicula->getDirector() == $nombreDirector && !in_array($pelicula, $peliculasEncontradas)) {
                 $peliculasEncontradas[] = $pelicula;
                 echo "En el cine " . $cine->getNombre() . " se encontró la película: " . $pelicula->getNombrePelicula() . " dirigida por " . $nombreDirector . ".\n";
@@ -73,4 +77,34 @@ function buscarPorDirector($cines, $nombreDirector) {
         }
     }
 }
+
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menú</title>
+</head>
+<body>
+    <form action="menu.php" method="post">
+        <label>Seleccione qué acción quiere realizar</label>
+        <p></p>
+        <select name="opcion">
+            <option value="Salir de la aplicación">Salir de la aplicación</option>
+            <option value="Mostrar peliculas">Mostrar peliculas</option>
+            <option value="Información de la pelicula más larga">Información de la pelicula más larga</option>
+            <option value="Buscar pelicula por director">Buscar pelicula por director</option>
+        </select>
+        <select name="director">
+            <option value="Edward Bazalgette">Edward Bazalgette</option>
+            <option value="David Wachtenheim">David Wachtenheim</option>
+            <option value="Lucía Alemany">Lucía Alemany</option>
+            <option value="Christopher Nolan">Christopher Nolan</option>
+        </select>
+        <p></p>
+        <button type="submit">Confirmar</button>
+    </form>
+</body>
+</html>
--- Nivel 3/pelicula.php
+++ Nivel 3/pelicula.php
@@ -23,5 +23,5 @@
     public function getDirector(){
         return $this -> director;
     }
-
+}
 ?>
